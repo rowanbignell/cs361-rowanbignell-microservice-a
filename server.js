4615d938529607ca93b47b421dc819017985dd50
Rowan Bignell <rowanbignell@gmail.com>
2025-02-22 14:41:09 -0800

    retrieve all food log info

--- server.js
+++ server.js
@@ -1,27 +1,37 @@
 var path = require('path')
 var express = require('express')
+var { customAlphabet } = require('nanoid');
+var alphabet = '123456789ABCDEFGHIJKLMNPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+var nanoid = customAlphabet(alphabet, 10);
+
+var foodData = require('./food-log.json')
 
 var app = express()
 var port = process.env.PORT || 3005
 
 app.use(express.json())
 
+// Retrieve all food log information
 app.get('/foodlog', function(req, res, next){
-    
+  res.status(200).json(foodData)
 })
 
+// Retrieve a single food log item
 app.get('/foodlog/:log', function(req, res, next){
     
 })
 
+// Add a single food log item
 app.post('/foodlog', function(req, res, next){
 
 })
 
+// Edit a single food log item
 app.put('/foodlog/:log', function(req, res, next){
 
 })
 
+// Delete a food log item
 app.delete('/foodlog/:log', function(req, res, next){
     
 })
